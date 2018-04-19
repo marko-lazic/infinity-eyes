e146a9868ff4d9f654d30a21f3ac05bb24425c42
Marko Lazic <zila015@gmail.com>
2018-04-19 21:05:27 +0400

    Added client side Counting web app implementation.

--- src/main/ui/src/app/app.component.ts
+++ src/main/ui/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'InfinityEyes';
+  result = '';
+
+  constructor(private http: HttpClient) {}
+
+  onKey(event: any) {
+    this.http.post('/counter', {'text': event.target.value})
+      .subscribe(data =>
+        this.result = "We counted " + data.toString() + " character I's for You.");
+  }
 }
